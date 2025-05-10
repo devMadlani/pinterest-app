edbf0165f141b963ee91aeebda8d3b078faff50b
Dev Madlani <madlanidev@gmail.com>
2025-05-10 17:12:06 +0530

    seed the initial data

--- server/index.js
+++ server/index.js
@@ -1,10 +1,23 @@
 import express from "express";
 import userRouter from "./routes/user.route.js";
+import pinRouter from "./routes/pin.route.js";
+import commentRouter from "./routes/comment.route.js";
+import boardRouter from "./routes/board.route.js";
 import connetDB from "./utils/connectDB.js";
-
+import cors from "cors";
 const app = express();
 
+app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+  })
+);
+
 app.use("/users", userRouter);
+app.use("/pins", pinRouter);
+app.use("/comments", commentRouter);
+app.use("/boards", boardRouter);
 
 app.listen(3000, () => {
   connetDB();
