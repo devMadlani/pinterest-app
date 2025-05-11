51540205d2a4900c8551ae5294168ddd36289711
Dev Madlani <madlanidev@gmail.com>
2025-05-11 12:14:37 +0530

    created get user and get pin route

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,3 +1,7 @@
-export const test = (req, res) => {
-  res.send("test");
+import User from "../models/user.model.js";
+
+export const getUser = async (req, res) => {
+  const { username } = req.params;
+  const user = await User.findOne({ username }).select("-hashedPassword");
+  res.status(200).json(user);
 };
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,24 +1,8 @@
 import express from "express";
-import { test } from "../controllers/user.controller.js";
-import User from "../models/user.model.js";
-import bcrypt from "bcryptjs";
+import { getUser } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
-router.post("/create", async (req, res) => {
-  const userInformation = req.body;
-  const hashedPassword = await bcrypt.hash(req.body.password, 10);
-  console.log(userInformation);
-  await User.updateOne({ username: "dev" }, req.body);
-  await User.create({
-    displayName: req.body.displayName,
-    username: req.body.username,
-    email: req.body.email,
-    hashedPassword: hashedPassword,
-  });
-  res.json("User Created");
-});
-
-router.get("/test", test);
+router.get("/:username", getUser);
 
 export default router;
