d2251665d05ca4f73d2eec1ba304c13ce925f104
Dev Madlani <madlanidev@gmail.com>
2025-05-11 14:34:50 +0530

    created getcomment routes

--- server/controllers/comment.controller.js
+++ server/controllers/comment.controller.js
@@ -1,3 +1,9 @@
-export const comment = (req, res) => {
-  res.send("test");
+import Comment from "../models/comment.model.js";
+import User from "../models/user.model.js";
+export const getPostComment = async (req, res) => {
+  const { postId } = req.params;
+  const comments = await Comment.find({ pin: postId })
+    .populate("user", "username img displayName")
+    .sort({ createdAt: -1 });
+  res.status(200).json(comments);
 };
--- server/routes/comment.route.js
+++ server/routes/comment.route.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { comment } from "../controllers/comment.controller.js";
+import { getPostComment } from "../controllers/comment.controller.js";
 
 const router = express.Router();
 
-router.get("/comment", comment);
+router.get("/:postId", getPostComment);
 
 export default router;
